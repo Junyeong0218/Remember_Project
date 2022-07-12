c31887085d6eb4e76eb97cae82d33a42c63f5b20
Junyeong0218 <hippo2003@naver.com>
2022-07-12 12:44:08 +0900

    console log test1

--- src/main/java/com/remember/app/config/BeanConfig.java
+++ src/main/java/com/remember/app/config/BeanConfig.java
@@ -4,8 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.remember.app.filter.FilePathFilter;
 
 @Configuration
 public class BeanConfig {
@@ -29,5 +32,13 @@ public class BeanConfig {
 	@Bean
 	public String modulePath() {
 		return modulePath;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<?> filter() {
+		FilterRegistrationBean<FilePathFilter> bean = new FilterRegistrationBean<FilePathFilter>(new FilePathFilter());
+		bean.addUrlPatterns("/**");
+		bean.setOrder(1);
+		return bean;
 	}
 }
---
+++ src/main/java/com/remember/app/filter/FilePathFilter.java
@@ -0,0 +1,22 @@
+package com.remember.app.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+public class FilePathFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		
+		System.out.println(req.getServletContext().getRealPath("/file_upload/"));
+	}
+
+}
